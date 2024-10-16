caad7993c16675e56453642fc7c6cd762d29dcd3
vivk3011 <31813149+vivk3011@users.noreply.github.com>
2024-10-16 15:52:11 +0530

    Update next.config.js German language code

--- next.config.js
+++ next.config.js
@@ -21,7 +21,7 @@ const nextConfig = {
   i18n: {
     // These are all the locales you want to support in your application.
     // These should generally match (or at least be a subset of) those in Sitecore.
-    locales: ['en'],
+    locales: ['en', 'de-De'],
     // This is the locale that will be used when visiting a non-locale
     // prefixed path e.g. `/styleguide`.
     defaultLocale: jssConfig.defaultLanguage,
